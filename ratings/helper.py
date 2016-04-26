3f3fd072d2568dbe5a23b7eb750034d5423dc81a
Skywalker.ljc <skywalker.ljc@gmail.com>
2016-04-26 13:05:37 -0400

    Live word clouds

--- ratings/helper.py
+++ ratings/helper.py
@@ -19,8 +19,40 @@ def find_movie(name, year, movies):
         if movie["name"] == name and movie["year"] == year:
             return movie
     return "No movie found!"     
+
+# Give a value and its old range, linear mapping to a new range as an integer
+def mapping(value, from_low, from_high, to_low, to_high):
+    new_value = float(value-from_low)/(from_high-from_low) * (to_high-to_low) + to_low
+    return int(new_value)
+
+# Given a dictionary of word-weights, return top k words with
+# adjusted weight for word cloud according to_low, to_high
+def top_words(top_dic, k, to_low, to_high): 
+    if k > len(top_dic): # k needs to be at most length of dic keys
+        k = len(top_dic)
     
-def find_similar_3(query):
+    temp = []
+    temp_weights = []
+    for word_weight in top_dic:
+        temp.append((word_weight[0], float(word_weight[1])))
+        temp_weights.append(float(word_weight[1]))
+    temp.sort(key = lambda x: x[1], reverse = True) # descending sort by value
+    from_low = min(temp_weights)
+    from_high = max(temp_weights)
+    
+    temp_k = []
+    for i in range(k): # get the top k value and adjust word size
+        temp_k.append((temp[i][0], mapping(temp[i][1], from_low, from_high, to_low, to_high)))
+    
+    # Create a new dic
+    frequency_list = []
+    for word_weight in temp_k:
+        temp = [word_weight[0], word_weight[1]]
+        frequency_list.append(temp)
+    
+    return frequency_list
+    
+def find_similar_3(query, k=30):
     # we store all movie information in one file, so just need to read first file
     # first file will be automatically named with id = 1 by Django
     movies = read_file(1)
@@ -46,5 +78,6 @@ def find_similar_3(query):
         result[count][1] = movie['rating_predict'] # get predicted ratings
         result[count][2] = movie['rating_actual'] # get real ratings
         result[count][3] = movie['comment'] # get the words for predicting ratings
+        result[count][4] = top_words(movie['top_words'], k, 15, 50) # re-arrane the top words
         count += 1
-    return result
+    return result, result[0][4], result[1][4], result[2][4]
--- ratings/views.py
+++ ratings/views.py
@@ -12,8 +12,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     movie_list = []
+    freq_1 = []
+    freq_2 = []
+    freq_3 = []
     search = "No query input yet"
     if request.GET.get('search'):
         search = request.GET.get('search')
-        movie_list = find_similar_3(search) # messaged sorted by distance compared to search_query
-    return render_to_response('movie_ratings/index.html', {'movie_list': movie_list,'input_query':search,})
+        movie_list,freq_1, freq_2, freq_3 = find_similar_3(search, 30) # messaged sorted by distance compared to search_query
+    return render_to_response('movie_ratings/index.html', \
+                             {'movie_list': movie_list,\
+                             'input_query':search,\
+                             'freq_1':freq_1,\
+                             'freq_2':freq_2,\
+                             'freq_3':freq_3,})
+
